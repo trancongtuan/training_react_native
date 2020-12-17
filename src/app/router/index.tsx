49051cb41cb79c0f6fa9f58f38acfc157855a899
tuan.tc <tuan.tc@neo-lab.vn>
2020-12-17 06:43:09 -0300

    add tabbar in router

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Router, Scene } from 'react-native-router-flux'
 import Home from '../view/home/home'
 import New from '../view/news/news'
+import TabBar from '../common/components/TabBar'
 
 const Routes = () => (
     <Router>
         <Scene key="root">
-            <Scene key="home" component={Home} title="TMDB" initial={true} />
-            <Scene key="news" component={New} title="News" />
+            <Scene key="home" component={TabBar} initial={true} hideNavBar={true}/>
+            <Scene key="news" component={New} title="News" hideNavBar={true} />
         </Scene>
     </Router>
 )
